51fcaca016aa250afa4abc8fe9cddcdb3dd5e5a1
PedroD <nevesdd01@outlook.com>
2022-12-09 20:14:06 -0500

    second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors  = require("cors")
 
 const moviesRouter = require("./movies/movies.router")
 const reviewsRouter = require("./reviews/reviews.router")
@@ -9,6 +10,7 @@ const notFound = require("./errors/notFound")
 const errorHandler = require("./errors/errorHandler")
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
